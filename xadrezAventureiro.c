80e783fdf6be12a3d4e773f0710df0171ab81797
Pedro Santos <106438638+ZziggP@users.noreply.github.com>
2025-10-03 12:49:12 -0300

    desafio nivel aventureiro xadrez

---
+++ xadrezAventureiro.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+
+//Declaração das funções
+void funcaotorre ();
+void funcaobispo ();
+void funcaorainha ();
+void funcaocavalo ();
+
+int main() {
+
+    //Chamando as funções
+    funcaotorre ();
+    printf("\n");
+    funcaobispo ();
+    printf("\n");
+    funcaorainha ();
+    printf("\n");
+    funcaocavalo ();
+
+    return 0;
+}
+
+//Função de repetição referente ao movimento da torre
+void funcaotorre (){
+
+    //declaração de variáveis
+    int torre, casas;
+    int totalCasas = 0;
+
+    //laço de repetição referente ao movimento da torre
+    printf("Movimento da torre:\n");
+    for (casas = 1; casas <= 5; casas++) {
+
+        printf("Direita\n");
+        totalCasas++;
+    }
+    //Declarando o numero total de casas percorridas pela torre
+    printf("O total de casas percorridas pela torre foi de %d casas para a direita.\n", totalCasas);
+}
+
+//Função de repetição referente ao movimento do bispo
+void funcaobispo (){
+
+    //declaração de variáveis
+    int bispo, casas;
+    int totalCasas = 0;
+
+    //inicializando a variável casas e totalCasas
+    casas = 1;
+    totalCasas = 0;
+    //laço de repetição referente ao movimento do bispo
+    printf("Movimento do bispo:\n");
+    do{
+
+        printf("Cima, Direita\n");
+        casas++;
+        totalCasas++;
+
+    } while (casas <= 5);
+
+    //Declarando o numero total de casas percorridas pelo bispo
+    printf("O total de casas percorridas pelo bispo foi de %d casas na diagonal cima, direita.\n", totalCasas);
+
+}
+
+//Função de repetição referente ao movimento da rainha
+void funcaorainha (){
+
+    //declaração de variáveis
+    int rainha, casas;
+    int totalCasas = 0;
+
+    //inicializando a variável casas e totalCasas
+    casas = 1;
+    totalCasas = 0;
+
+    //laço de repetição referente ao movimento da rainha
+    printf("Movimento da rainha:\n");
+    while (casas <= 8){
+
+        printf("Esquerda\n");
+        casas++;
+        totalCasas++;
+    }
+    //Declarando o numero total de casas percorridas pela rainha
+    printf("O total de casas percorridas pela rainha foi de %d casas para a esquerda.\n", totalCasas);
+
+}
+
+//Funcao de repeticao referente ao movimento do cavalo
+void funcaocavalo (){
+
+    //Decalaração de variáveis
+    int cavalo = 1;
+    int totalCasasEsquerda = 0;
+    int totalCasasBaixo = 0;
+    
+    //Laço de repetição referente ao movimento do cavalo
+    printf("Movimento do cavalo:\n");
+    while (cavalo--){
+
+        for (int casas = 0; casas < 2; casas++){
+
+            printf("Baixo, ");
+            totalCasasBaixo++;
+
+        }
+        printf("Esquerda.\n");
+        totalCasasEsquerda++;
+
+    }
+    printf("O total de casas percorridas pelo cavalo foi de %d casas para baixo e %d casas para a esquerda.\n", totalCasasBaixo, totalCasasEsquerda);
+
+}
